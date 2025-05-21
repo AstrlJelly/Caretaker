b448f538e57d8a7cf0dd70a3c8c8ab3b2dbd31d8
AstrlJelly <bdlawson115@gmail.com>
2025-05-21 16:58:06 -0400

    initialize project! :D

---
+++ Init.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Caretaker
+{
+    class Init
+    {
+        private static void Main()
+        {
+
+        } 
+    }
+}
